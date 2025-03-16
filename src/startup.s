c954e85e91345cb16487654a8c85aa27ddc56dc9
lnxknl4 <jjmutong@gmail.com>
2025-03-16 11:06:53 +0800

    update

--- src/startup.s
+++ src/startup.s
@@ -5,28 +5,39 @@
 .global _vectors
 
 _vectors:
-    ldr pc, =_start           @ Reset
-    ldr pc, =_undefined       @ Undefined instruction
-    ldr pc, =_svc            @ Software interrupt
-    ldr pc, =_prefetch       @ Prefetch abort
-    ldr pc, =_data           @ Data abort
-    ldr pc, =_unused         @ Not used
-    ldr pc, =_irq           @ IRQ interrupt
-    ldr pc, =_fiq           @ FIQ interrupt
+    b _start              @ Reset
+    b _undefined          @ Undefined instruction
+    b _svc               @ Software interrupt
+    b _prefetch          @ Prefetch abort
+    b _data              @ Data abort
+    b _unused            @ Not used
+    b _irq               @ IRQ interrupt
+    b _fiq               @ FIQ interrupt
 
 .section .text
 .align 4
 
 _start:
-    @ 设置处理器模式为SVC模式
+    @ 设置处理器模式为SVC模式，禁用所有中断
     mrs r0, cpsr
     bic r0, r0, #0x1F
     orr r0, r0, #0x13
+    orr r0, r0, #0xC0    @ 禁用 IRQ 和 FIQ
     msr cpsr, r0
 
     @ 初始化栈指针
     ldr r0, =_stack_top
     mov sp, r0
+
+    @ 复制 .data 段到 RAM
+    ldr r0, =_data_loadaddr
+    ldr r1, =_data_start
+    ldr r2, =_data_end
+copy_data:
+    cmp r1, r2
+    ldrlt r3, [r0], #4
+    strlt r3, [r1], #4
+    blt copy_data
 
     @ 清零 BSS 段
     ldr r0, =__bss_start
